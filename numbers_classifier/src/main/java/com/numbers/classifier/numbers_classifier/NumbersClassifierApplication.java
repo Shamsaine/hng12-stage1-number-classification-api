a223bead80890d73250a1e498eea3a725b06bbc5
Shamsudeen Ibrahim <shamsain3@gmail.com>
2025-02-06 14:24:44 +0100

    Added A springboot boilerplate

---
+++ numbers_classifier/src/main/java/com/numbers/classifier/numbers_classifier/NumbersClassifierApplication.java
@@ -0,0 +1,13 @@
+package com.numbers.classifier.numbers_classifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbersClassifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbersClassifierApplication.class, args);
+	}
+
+}
---
+++ numbers_classifier/src/test/java/com/numbers/classifier/numbers_classifier/NumbersClassifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numbers.classifier.numbers_classifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbersClassifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
